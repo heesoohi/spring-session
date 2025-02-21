e375aac9efcabe5b969ee8ab082e39167d70600b
이희수 <leehsdd33@gmail.com>
2025-02-21 20:08:40 +0900

    feat: 할일 저장 기능 구현 #8

--- src/main/java/com/example/springsession1/todo/controller/TodoController.java
+++ src/main/java/com/example/springsession1/todo/controller/TodoController.java
@@ -1,12 +1,27 @@
 package com.example.springsession1.todo.controller;
 
+import com.example.springsession1.common.consts.Const;
+import com.example.springsession1.todo.dto.TodoSaveRequestDto;
+import com.example.springsession1.todo.dto.TodoSaveResponseDto;
 import com.example.springsession1.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
 
     private final TodoService todoService;
+
+    @PostMapping("/todos")
+    public ResponseEntity<TodoSaveResponseDto> saveTodo(
+            @SessionAttribute(name = Const.LOGIN_MEMBER) Long memberId,
+            @RequestBody TodoSaveRequestDto dto
+    ){
+        return ResponseEntity.ok(todoService.saveTodo(memberId, dto));
+    }
 }
--- src/main/java/com/example/springsession1/todo/service/TodoService.java
+++ src/main/java/com/example/springsession1/todo/service/TodoService.java
@@ -1,6 +1,10 @@
 package com.example.springsession1.todo.service;
 
+import com.example.springsession1.member.entity.Member;
 import com.example.springsession1.member.repository.MemberRepository;
+import com.example.springsession1.todo.dto.TodoSaveRequestDto;
+import com.example.springsession1.todo.dto.TodoSaveResponseDto;
+import com.example.springsession1.todo.entity.Todo;
 import com.example.springsession1.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,5 +16,14 @@ public class TodoService {
     private final TodoRepository todoRepository;
     private final MemberRepository memberRepository;
 
+    public TodoSaveResponseDto saveTodo(Long memberId, TodoSaveRequestDto dto) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new IllegalStateException("Member not found with id " + memberId)
+        );
 
+        Todo todo = new Todo(dto.getContent(), member);
+        Todo savedTodo = todoRepository.save(todo);
+
+        return new TodoSaveResponseDto(savedTodo.getId(), member.getId(), savedTodo.getContent());
+    }
 }
