865c98bbedc4e322ab49cd06ce539a5758a609a4
이희수 <leehsdd33@gmail.com>
2025-02-21 20:24:56 +0900

    feat: 할일 다건/단건 조회 기능 구현 #9

--- src/main/java/com/example/springsession1/todo/controller/TodoController.java
+++ src/main/java/com/example/springsession1/todo/controller/TodoController.java
@@ -1,15 +1,15 @@
 package com.example.springsession1.todo.controller;
 
 import com.example.springsession1.common.consts.Const;
+import com.example.springsession1.todo.dto.TodoResponseDto;
 import com.example.springsession1.todo.dto.TodoSaveRequestDto;
 import com.example.springsession1.todo.dto.TodoSaveResponseDto;
 import com.example.springsession1.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,4 +24,15 @@ public class TodoController {
     ){
         return ResponseEntity.ok(todoService.saveTodo(memberId, dto));
     }
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<TodoResponseDto>> getAll(){
+        return ResponseEntity.ok(todoService.findAll());
+    }
+
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity<TodoResponseDto> getOne(@PathVariable Long todoId){
+        return ResponseEntity.ok(todoService.findById(todoId));
+    }
 }
+
--- src/main/java/com/example/springsession1/todo/service/TodoService.java
+++ src/main/java/com/example/springsession1/todo/service/TodoService.java
@@ -2,12 +2,15 @@ package com.example.springsession1.todo.service;
 
 import com.example.springsession1.member.entity.Member;
 import com.example.springsession1.member.repository.MemberRepository;
+import com.example.springsession1.todo.dto.TodoResponseDto;
 import com.example.springsession1.todo.dto.TodoSaveRequestDto;
 import com.example.springsession1.todo.dto.TodoSaveResponseDto;
 import com.example.springsession1.todo.entity.Todo;
 import com.example.springsession1.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +28,19 @@ public class TodoService {
         Todo savedTodo = todoRepository.save(todo);
 
         return new TodoSaveResponseDto(savedTodo.getId(), member.getId(), savedTodo.getContent());
+    }
+
+    public List<TodoResponseDto> findAll() {
+        return todoRepository.findAll().stream()
+                .map( todo -> new TodoResponseDto(todo.getId(), todo.getContent()))
+                .toList();
+    }
+
+    public TodoResponseDto findById(Long todoId) {
+        Todo todo = todoRepository.findById(todoId).orElseThrow(
+                () -> new IllegalStateException("Todo not found")
+        );
+
+        return new TodoResponseDto(todo.getId(), todo.getContent());
     }
 }
